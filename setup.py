9ce594da8aa09451bbaa50f52c8a3a3867a38b8e
Laurent Dufresne <laurent.dufresne@hotmail.com>
2019-04-04 19:57:55 +0200

    Made a proper repository for the module

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='process',
+    version='1.0',
+    description='Python module to interact with running processes on Windows',
+    author='Laurent Dufresne',
+    license='MIT',
+    py_modules=['process'],
+    url = 'https://github.com/reduf/process'
+)
